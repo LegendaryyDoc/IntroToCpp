bcfeba72ec019ec1ecd9486f1cce54c149f67f1f
Nathan Knebusch <nateknebusch@gmail.com>
2018-08-16 12:49:22 -0700

    Add initial project files

---
+++ 1-HelloAIE/IntroToCpp.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+int main()
+{
+
+std::cout << "Hello AIE\n" << std::endl;
+
+	// tracklist
+	std::cout << "Red like Roses" << std::endl;
+	std::cout << "Halo 3 combat soundtrack" << std::endl;
+	std::cout << "Love Sosa" << std::endl;
+	std::cout << "Often" << std::endl;
+	std::cout << "Sleepwalker\n" << std::endl;
+
+	// Numbers
+	std::cout << "0" << std::endl;
+	std::cout << "1" << std::endl;
+	std::cout << "2" << std::endl;
+	std::cout << "3" << std::endl;
+	std::cout << "4" << std::endl;
+	std::cout << "5\n" << std::endl;
+
+	std::cout << "5" << std::endl;
+	std::cout << "4" << std::endl;
+	std::cout << "3" << std::endl;
+	std::cout << "2" << std::endl;
+	std::cout << "1" << std::endl;
+	std::cout << "0" << std::endl;
+
+	system("pause");
+}
---
+++ 2-Variables/Main.cpp
@@ -0,0 +1,163 @@
+#include <iostream>
+
+int main()
+{
+	//std::cout << "It's the second project! :D" << std::endl;
+
+	//// print a variable to the console
+	//int weightFloatKg = 46.72f;
+
+	//std::cout << "Here is my weight:" << weightFloatKg << "kg" << std::endl;
+
+	//// prompt the user for their weight
+	//int userWeightKg = -1;
+
+	//std::cout << "What is your weight in kilograms, friend" << std::endl;
+	//std::cin >> userWeightKg;
+
+	//std::cout << "Your weight is "  << std::endl; 
+
+
+	////A
+	//int numberA = 5;
+
+	//numberA = 9;
+	//numberA = 11; 
+	//numberA = 14;
+
+	//std::cout << "A)" << numberA << std::endl;
+
+	//// B
+	//int numberB = 10;
+	//numberB = 9; 
+
+	//int thingB = 55; 
+	//thingB = 22;
+
+	//std::cout << "B)" << numberB << std::endl;
+
+	//// C
+	//int numberC = 3;
+	//numberC = 7;
+	//numberC = 1;
+
+	//int somethingC = 23;
+	//somethingC = 21;
+
+	//std::cout << "C)" << numberC << std::endl;
+
+	//// D
+	//int numberD = 1;
+	//int somethingD = 3;
+
+	//numberD = somethingD;
+
+	//std::cout << "D)" << somethingD << std::endl;
+
+	//// E
+	//int x = 13;
+
+	//x = x / 2;
+
+	//std::cout << "E)" << x << std::endl;
+
+	//// F
+	//float y = 13;
+
+	//y = y / 2;
+
+	//std::cout << "F)" << y << std::endl;
+
+
+	//// Celcius to Fahrenheit
+	//float degCelcius = 78.0f;
+	//float degFahrenheit = 0.0f;
+
+	//degFahrenheit = degCelcius * 2.21f;
+
+	//std::cout << "Celcius to Fahrenheit)" << std::endl;
+	//std::cout << "Celcius:" << degCelcius << std::endl;
+	//std::cout << "Fahrenheit:" << degFahrenheit << std::endl;
+
+	//// prompt user to input celcius
+	//float userDegCelcius = 0.0f;
+	//float userDegFahrenheit = 0.0f;
+
+	//std::cout << "What is the temperature in Celcius?" << std::endl;
+	//std::cin >> userDegCelcius;
+
+	//std::cout << "The temperature in Fahrenheit is " << userDegCelcius * 1.8f +32 << std::endl;	
+
+	//// Area of a Rectangle
+	//float rectWidth = 0.0f;
+	//float rectHeight = 0.0f;
+	//float rectArea = 0.0f; 
+
+	//std::cout << "What is the width of the rectangle?" << std::endl;
+	//std::cin >> rectWidth;
+
+	//std::cout << "What is the height of the rectangle?" << std::endl;
+	//std::cin >> rectHeight;
+
+	//std::cout << "The area of the rectangle is " << rectWidth * rectHeight << std::endl;
+
+	//// Average of Five
+	//float personA = 0.0f;
+	//float personB = 0.0f;
+	//float personC = 0.0f;
+	//float personD = 0.0f;
+	//float personE = 0.0f;
+	//float avg = 0.0f; 
+
+	//std::cout << "What is age a?" << std::endl;
+	//std::cin >> personA;
+
+	//std::cout << "What is age b?" << std::endl;
+	//std::cin >> personB;
+
+	//std::cout << "What is age c?" << std::endl;
+	//std::cin >> personC;
+
+	//std::cout << "What is age d?" << std::endl;
+	//std::cin >> personD;
+
+	//std::cout << "What is age e?" << std::endl;
+	//std::cin >> personE;
+
+	//float sum = personA + personB + personC + personD + personE;
+	//avg = sum / 5.0f;
+
+	//std::cout << "The average age is " << avg << std::endl;
+
+	//// Number Swap
+	//float numberX = 0.0f;
+	//float numberY = 0.0f;
+
+	//std::cout << "What is number for x?" << std::endl;
+	//std::cin >> numberX; 
+
+	//float newY;
+	//newY = numberX;
+
+	//std::cout << "What is the number for y?" << std::endl;
+	//std::cin >> numberY; 
+
+	//float newX;
+	//newX = numberY;
+	//
+	//std::cout << "Number Swap)" << std::endl;
+	//std::cout << "x is " << newX << std::endl;
+	//std::cout << "y is " << newY << std::endl; 
+
+	// Fun Facts for Your Age
+	int age = -1;
+
+	std::cout << "How old are you in years?" << std::endl;
+	std::cin >> age;
+
+	std::cout << "You've been alive for " << age << "years." << std::endl;
+	std::cout << "That's " << age / 10 << "decades!" << std::endl;
+	std::cout << "That's " << age / 20 << "scores!" << std::endl;
+	
+	system("pause");
+}
